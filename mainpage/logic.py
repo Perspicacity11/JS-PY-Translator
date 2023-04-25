7bbd3b3012dd05b21877adc58e457f1a20f9fb0a
Perspicacity11 <cassius.cr.naylor@gmail.com>
2023-04-25 09:58:15 +0100

    Added command line API interface logic for programme to logic.py

--- mainpage/logic.py
+++ mainpage/logic.py
@@ -1,9 +1,10 @@
 import openai
-openai.api_key = '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'
+openai.api_key = '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' # GET A NEW KEY AS THIS ONE WAS DEACTIVATED (CAN'T BE PUBLIC IN COMMITS)
 messages = [ {"role": "system", "content":
               "You are an intelligent assistant."} ]
 while True:
     message = input("User : ")
+    # I need to configure this message variable to receive form input from the view
     if message:
         messages.append(
             {"role": "user", "content": message},
@@ -13,4 +14,5 @@ while True:
         )
     reply = chat.choices[0].message.content
     print(f"ChatGPT: {reply}")
+    # I need to output the reply variable into the view
     messages.append({"role": "assistant", "content": reply})
--- mainpage/views.py
+++ mainpage/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+# I need to provide the logic from logic.py as a third argument to this render (so I'll need to import the logic file)
