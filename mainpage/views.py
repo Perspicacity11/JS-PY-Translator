67e80450077c781d5f6abfdf83b01db75b45e7b9
Perspicacity11 <cassius.cr.naylor@gmail.com>
2023-04-25 11:40:37 +0100

    Built CL logic into view file, moved api key to .env

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,8 +1,27 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
+import openai, os
+from dotenv import load_dotenv
+load_dotenv()
+
+api_key = os.getenv("OPENAI_KEY", None)
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    chatbot_response = None
+    if api_key is not None and if request.method == 'POST':
+        openai.api_key = api_key
+        user_input = request.POST.get('user_input')
+        prompt = user_input
+
+        response = openai.Completion.create(
+            engine = 'text-davinci-003',
+            prompt = prompt,
+            max_tokens = 256,
+            # stop = '.' #update this as this represents the end of the prompt (a full stop in this case)
+            temperature = 0.5,
+        )
+        print(response)
+    return render(request, 'index.html', {})
 
 # I need to provide the logic from logic.py as a third argument to this render (so I'll need to import the logic file)
