84fdd3dbcbf73a1ead6628eec1a6be93473ab5df
Perspicacity11 <cassius.cr.naylor@gmail.com>
2023-05-09 21:40:28 +0100

    Added requirements.txt

--- mainpage/tests.py
+++ mainpage/tests.py
@@ -1,3 +1,68 @@
 from django.test import TestCase
 
 # Create your tests here.
+import unittest
+from .views import index
+
+class TestJS2PYTranslator(unittest.TestCase):
+
+# I need to mock the API request fully, including the POST method
+
+    def test_empty_string(self):
+        result = index("")
+        self.assertEqual(result, "")
+
+    def test_single_line_assignment(self):
+        js_code = "var a = 5;"
+        py_code = "a = 5"
+        result = index(js_code)
+        self.assertEqual(result, py_code)
+
+    def test_single_line_function(self):
+        js_code = "function add(a, b) { return a + b; }"
+        py_code = "def add(a, b):\n    return a + b"
+        result = index(js_code)
+        self.assertEqual(result, py_code)
+
+    def test_multi_line_function(self):
+        js_code = """
+        function factorial(n) {
+            if (n <= 1) {
+                return 1;
+            } else {
+                return n * factorial(n - 1);
+            }
+        }
+        """
+        py_code = """
+        def factorial(n):
+            if n <= 1:
+            return 1
+        else:
+        return n * factorial(n - 1)
+        """
+        result = index(js_code)
+        self.assertEqual(result, py_code)
+
+    def test_conditional_statement(self):
+        js_code = """
+        var age = 18;
+        if (age >= 18) {
+            console.log("You are an adult.");
+        } else {
+            console.log("You are a minor.");
+        }
+        """
+        py_code = """
+        age = 18
+            if age >= 18:
+            print("You are an adult.")
+        else:
+            print("You are a minor.")
+        """
+        result = index(js_code)
+        self.assertEqual(result, py_code)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- mainpage/views.py
+++ mainpage/views.py
@@ -5,8 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 api_key = os.getenv("OPENAI_KEY", None)
-
-# Create your views here.
 def index(request):
     chatbot_response = None
     if api_key is not None and request.method == 'POST':
@@ -14,6 +12,11 @@ def index(request):
         user_input = request.POST.get('user_input')
         prompt = f"""Please translate this JavaScript code into Python, and ensure that the response is formatted as 
         code with appropriate indentation: {user_input}"""
+
+        # Can I write a conditional version of this prompt, which first detects if the input code is JS or PY,
+        # and then translates into the other with needing an additional prompt?
+        # That or I can have a tickbox in the view for JS-PY and PY-JS, and the state of that variable determines
+        # which prompt is used
 
         response = openai.Completion.create(
             engine = 'text-davinci-003',
@@ -26,5 +29,3 @@ def index(request):
 
         chatbot_response = response['choices'][0]['text']
     return render(request, 'index.html', {'response': chatbot_response})
-
-# I need to provide the logic from logic.py as a third argument to this render (so I'll need to import the logic file)
